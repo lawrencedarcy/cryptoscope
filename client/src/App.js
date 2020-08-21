3eb3c4411fae700bdca81193f991e40900254177
lawrencedarcy <lawrencedwakefield@gmail.com>
2020-08-21 10:59:39 +0100

    create app and navbar component

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './NavBar/NavBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      Yeh is cryptoscope
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar(props) {
+  return (
+    <div className='navbar-outer'>
+      <div className='navbar-inner'>
+        <div className='logo'>CRYPTOSCOPE</div>
+        
+      </div>
+    
+    </div>
+  );
+}
+
+export default NavBar;
