9f87e825a3bc5451bdb4fecf2a23b2cf90b71539
lawrencedarcy <lawrencedwakefield@gmail.com>
2020-08-21 13:45:39 +0100

    add axios and get request

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import NavBar from './NavBar/NavBar';
 import './App.css';
 import CandleChart from './CandleChart/CandleChart';
 import ButtonBar from './ButtonBar/ButtonBar'
 
 function App() {
+
+  axios.get('https://api.binance.com/api/v3/klines?symbol=BNBBTC&interval=1h&limit=1000', {
+    params: {
+      symbol: 'BNBBTC',
+      interval: 1,
+      limit: 1000
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
   return (
     <div className="App">
       <NavBar />
